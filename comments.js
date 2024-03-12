ad62ca0756b83d6eb629912f2da0192911415dd9
huynguyen2741 <33584122+huynguyen2741@users.noreply.github.com>
2024-03-12 04:31:17 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create web server
+// Start server
+// Load comments from file
+// Add new comments to file
+// Read comments from file
+// Send comments to client
+// Send form to client
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+
+// Create web server
