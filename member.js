29b3723fd75a13d61bb84222137bc7b3285ba7ff
huynguyen2741 <33584122+huynguyen2741@users.noreply.github.com>
2024-03-12 04:18:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillMembers() {
+  return {
+    name: 'skillMembers',
+    members: [
+      {
+        name: 'skillMembers',
+        type: 'function',
+        value: skillMembers
+      },
+      {
+        name: 'skillMembers',
+        type: 'function',
+        value: skillMembers
+      }
+    ]
+  }
+}
